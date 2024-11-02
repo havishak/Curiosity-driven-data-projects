1be53070eeb1762d3e9a5e1a3def3f45066e9b3d
havishak <88750950+havishak@users.noreply.github.com>
2024-11-01 18:08:48 -0700

    updated figure

--- tidytuesday/script/tt_oct2924.R
+++ tidytuesday/script/tt_oct2924.R
@@ -17,25 +17,25 @@ eyes_data <- data.frame(
     val = rep(0.4, 6)
 )
 
-theta <- seq(pi, 2*pi, length.out = 100) 
-smiley_shape1 <- data.frame(
-    cont_col = "year",
-    x = 0.3*cos(theta),
-    y = 0.3*sin(theta)
-)
-
-smiley_shape2 <- data.frame(
-    cont_col = "runtime_minutes",
-    x = seq(-2,2.5,0.5),
-    y = rep(c(0, 1),5)
-)
-
-
-# Plot the semi-circle using ggplot2
-plot_smiley1 <- ggplot(smiley_shape1, aes(x, y)) +
-    geom_polygon(fill = "gray30") +
-    coord_fixed()  +  # Set limits
-    theme_void()  # Remove axes
+# theta <- seq(pi, 2*pi, length.out = 100) 
+# smiley_shape1 <- data.frame(
+#     cont_col = "year",
+#     x = 0.3*cos(theta),
+#     y = 0.3*sin(theta)
+# )
+# 
+# smiley_shape2 <- data.frame(
+#     cont_col = "runtime_minutes",
+#     x = seq(-2,2.5,0.5),
+#     y = rep(c(0, 1),5)
+# )
+# 
+# 
+# # Plot the semi-circle using ggplot2
+# plot_smiley1 <- ggplot(smiley_shape1, aes(x, y)) +
+#     geom_polygon(fill = "gray30") +
+#     coord_fixed()  +  # Set limits
+#     theme_void()  # Remove axes
 
 ggplot() +
     geom_polygon(data = smiley, aes(x = x, y = y), fill = "orange", color = "black") +
@@ -46,7 +46,7 @@ ggplot() +
 
 # I want to show all continuous variables as pumpkins
 set.seed(1)
-plot_partial <- monster_movies |>
+monster_movies |>
     mutate(across(c(year, runtime_minutes, average_rating, num_votes), ~(.x - mean(.x, na.rm = T))/sd(.x, na.rm = T))) |>
     pivot_longer(
         cols = c(year, runtime_minutes, average_rating),
@@ -60,15 +60,23 @@ plot_partial <- monster_movies |>
     geom_quasirandom(method = "smiley", aes(color = val > 0.9)) +
     # add eyes
     geom_quasirandom(data = eyes_data, 
-               aes(x = cont_col, y = val, shape = cont_col), method = "tukeyDense", size = 6, color = "darkred", fill = "gray40")+
+               aes(x = cont_col, y = val, shape = cont_col), method = "tukeyDense", size = 11, color = "black", fill = "gray50")+
     # add nose
-    geom_point(aes(x = cont_col, y = 0), size = 6, 
+    geom_point(aes(x = cont_col, y = -0.2), size = 8, 
                color = "gray20", fill = "gray20", shape = 18)+
     scale_shape_manual(
         values = c(8, 21, 24)
     ) +
     scale_color_manual(values = c("#FF7518", "#286848")) +   
     theme_void() +
+    labs(title = "TRICK OR TREAT?",
+         caption = "Source: IMBD Halloween data from TidyTuesday") +
     theme(
-        legend.position = "none"
+        legend.position = "none",
+        text = element_text(family = "Creepster"),
+        plot.title = element_text(hjust = 0.5, size = 100, color = "gray90"),
+        plot.caption = element_text(color = "gray90", size = 50),
+        plot.background = element_rect(fill = 'gray15')
     )
+
+ggsave("products/tt_oct29_halloween.png")
